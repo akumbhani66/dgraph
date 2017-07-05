af9e074df1ffba320158d24cf842cad5d5208ac2
Janardhan Reddy <janardhan@dgraph.io>
2017-07-05 18:10:13 +1000

    Revert "use kv to store xid at client"
    This reverts commit 4b5d9af49ff6ed6e03f378332e96e8f73c99dd3e.

--- client/mutations.go
+++ client/mutations.go
@@ -18,12 +18,10 @@ package client
 
 import (
 	"context"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"log"
 	"math/rand"
-	"os"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -31,9 +29,6 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-
-	"github.com/dgraph-io/badger/badger"
-	"github.com/dgraph-io/badger/table"
 
 	"github.com/dgraph-io/dgraph/protos"
 	"github.com/dgraph-io/dgraph/x"
@@ -56,8 +51,10 @@ type BatchMutationOptions struct {
 type allocator struct {
 	x.SafeMutex
 
-	kv *badger.KV
 	dc protos.DgraphClient
+
+	// TODO: Evict older entries
+	ids map[string]uint64
 
 	startId uint64
 	endId   uint64
@@ -88,35 +85,19 @@ func (a *allocator) fetchOne() (uint64, error) {
 	return uid, nil
 }
 
-func (a *allocator) getFromKV(id string) (uint64, error) {
-	var item badger.KVItem
-	if err := a.kv.Get([]byte(id), &item); err != nil {
-		return 0, err
-	}
-	val := item.Value()
-	if len(val) > 0 {
-		uid, n := binary.Uvarint(val)
-		if n <= 0 {
-			return 0, fmt.Errorf("Unable to parse val %q to uint64 for %q", val, id)
-		}
-		return uid, nil
-	}
-	return 0, nil
-}
-
 func (a *allocator) assignOrGet(id string) (uid uint64, isNew bool,
 	err error) {
 	a.RLock()
-	uid, err = a.getFromKV(id)
+	uid = a.ids[id]
 	a.RUnlock()
-	if err != nil || uid > 0 {
+	if uid > 0 {
 		return
 	}
 
 	a.Lock()
 	defer a.Unlock()
-	uid, err = a.getFromKV(id)
-	if err != nil || uid > 0 {
+	uid = a.ids[id]
+	if uid > 0 {
 		return
 	}
 
@@ -124,9 +105,7 @@ func (a *allocator) assignOrGet(id string) (uid uint64, isNew bool,
 	if err != nil {
 		return
 	}
-	var buf [20]byte
-	n := binary.PutUvarint(buf[:], uid)
-	a.kv.Set([]byte(id), buf[:n])
+	a.ids[id] = uid
 	isNew = true
 	err = nil
 	return
@@ -164,21 +143,12 @@ type Dgraph struct {
 // NewBatchMutation is used to create a new batch.
 // size is the number of RDF's that are sent as part of one request to Dgraph.
 // pending is the number of concurrent requests to make to Dgraph server.
-func NewDgraphClient(conn *grpc.ClientConn, opts BatchMutationOptions,
-	clientDir string) *Dgraph {
+func NewDgraphClient(conn *grpc.ClientConn, opts BatchMutationOptions) *Dgraph {
 	client := protos.NewDgraphClient(conn)
-	x.Check(os.MkdirAll(clientDir, 0700))
-	opt := badger.DefaultOptions
-	opt.SyncWrites = false
-	opt.MapTablesTo = table.LoadToRAM
-	opt.Dir = clientDir
-	opt.ValueDir = clientDir
-	kv, err := badger.NewKV(&opt)
-	x.Checkf(err, "Error while creating badger KV posting store")
 
 	alloc := &allocator{
-		kv: kv,
-		dc: client,
+		dc:  client,
+		ids: make(map[string]uint64),
 	}
 	d := &Dgraph{
 		opts:   opts,
